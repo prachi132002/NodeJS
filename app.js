682455c7a7b1d891c8fce352e1e7cc0cf5acde52
findankit <122311036+findankit@users.noreply.github.com>
2023-04-22 17:08:39 +0530

    Express Initialization

--- app.js
+++ app.js
@@ -1,44 +1,17 @@
-const http = require("http");
+const express = require("express");
+const routes = require("./routes");
 
-let server = http.createServer(function (req, res) {
-  // apis here
-  let method = req.method;
-  let url = req.url;
+const app = express();
+const port = 3001;
 
-//   let body = "";
-//   req.on("data", (chunk) => {
-//     body += chunk;
-//   });
-
-  console.log(method, url);
-  if (method === "POST") {
-    if (url === "/intro") {
-      // db operations
-      // sending email
-    //   console.log(body);
-
-      res.write("Inside Intro.");
-    } else {
-      res.write("Api not found!");
-    }
-  } else {
-    res.write("Api not found!");
-  }
-
-  res.end();
+app.get("/hello-world", function (req, res) {
+  res.send("Hello World");
 });
 
-server.listen(3001, () => {
-  console.log(`Server started on port`, 3001);
+app.post("/hello-world", function (req, res) {
+  res.send("Hello World");
 });
 
-/**
- * method
- * header
- * payload (body)
- */
-
-let obj = {
-  name: "Test",
-  password: "passwrd",
-};
+app.listen(port, () => {
+  console.log(`Server started on `, port);
+});
